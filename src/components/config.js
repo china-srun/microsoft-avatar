44d4f408c3c02c665aba6d1f6b1a0a1c018039e9
china-srun <china.srunn@gmail.com>
2024-02-29 16:21:52 +0700

    first commit

---
+++ src/components/config.js
@@ -0,0 +1,12 @@
+
+export const avatarAppConfig = {
+    cogSvcRegion : "eastus",
+    cogSvcSubKey : "74c1bae5a9364d39bd8f3c5776bcd9d4",
+    voiceName : "en-US-JennyNeural",
+    avatarCharacter : "lisa",
+    avatarStyle : "casual-sitting",
+    avatarBackgroundColor : "#FFFFFFFF",
+    iceUrl : "stun:relay.communication.microsoft.com:3478",
+    iceUsername : "BQAANp0oFwAB2lxFAS+4RNZC3R8nGZQymc4uhy9lZ+AAAAAMARC4wekX4DxDAKF3ZxMTPvbHZ6r/4J5qG0cH1NJcOkIpWFrNNCk=",
+    iceCredential : "+hMOwWtq7OIL8E+0OdSK6mBdoAY="
+  }
